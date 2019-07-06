e7cc47a68fd5c6aa42af6f38808e038215209dfe
Error Event <1807657825@qq.com>
2019-07-06 10:17:24 +0800

    add andriod

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { Plugins, CameraResultType, CameraSource, PluginRegistry } from '@capacitor/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { Album } from "goodeventalbum";
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      // source: CameraSource.Camera
+    });
 
-  constructor() {}
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+  }
+  photo: SafeResourceUrl;
+  callJava(){
+    Album.echo({value:'xxx'})
+    .then(value => {
+      console.log(value)
+    })
+  }
+  constructor(private sanitizer: DomSanitizer) {  }
 
 }
